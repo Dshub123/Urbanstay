c8fc969c000f168ee003769277f5c36a2df5b013
Piyush Gandhi <gpiyush211.pg@gmail.com>
2023-03-14 19:13:00 +0530

    updates

--- src/components/Owner/Owner.js
+++ src/components/Owner/Owner.js
@@ -1,5 +1,4 @@
 import OwnerDashboard from "../../pages/DAshboard/OwnerDashboard";
-import UpdateProfile from "../../pages/User/UpdateProfile/updateProfile";
 import OwnerNavbar from "../Header/OwnerNav";
 
 export default function Owner(){
--- src/components/Room/searchItem/List.jsx
+++ src/components/Room/searchItem/List.jsx
@@ -1,13 +1,12 @@
 import "./list.css";
 import { useState } from "react";
-import { format } from "date-fns";
-import { DateRange } from "react-date-range";
+import { DatePicker, Space } from "antd";
 import SearchItem from "./SearchItem";
 
+const {RangePicker}=DatePicker;
 const List = () => {
 
-    const [openDate, setOpenDate] = useState(false);
-
+    // const [openDate, setOpenDate] = useState(false);
     return (
         <div>
 
@@ -38,33 +37,7 @@ const List = () => {
                                     </span>
                                     <input type="number" className="lsOptionInput" />
                                 </div>
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">Adult</span>
-                                    <input
-                                        type="number"
-                                        min={1}
-                                        className="lsOptionInput"
-                                        placeholder={"options.adult"}
-                                    />
-                                </div>
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">Children</span>
-                                    <input
-                                        type="number"
-                                        min={0}
-                                        className="lsOptionInput"
-                                        placeholder={"options.children"}
-                                    />
-                                </div>
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">Room</span>
-                                    <input
-                                        type="number"
-                                        min={1}
-                                        className="lsOptionInput"
-                                        placeholder={"options.room"}
-                                    />
-                                </div>
+                         
                             </div>
                         </div>
                         <button>Search</button>
